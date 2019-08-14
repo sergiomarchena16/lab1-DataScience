85395ac935e1b4152e6c0b513ca925aca856defc
Sergio Marchena <mar16387@uvg.edu.gt>
2019-08-14 13:58:05 -0600

    comienzo
    1, 2 (medio) y 3 y medio

--- lab1.R
+++ lab1.R
@@ -10,11 +10,68 @@
 datos <- read.csv("train.csv")
 
 # 1. Haga una exploración rápida de sus datos para eso haga un resumen de su dataset
+View(datos)
 summary(datos)
 str(datos)
-head(datos)
 
 # 2. Diga el tipo de cada una de las variables del dataset (cualitativa o categórica, cuantitativa continua, cuantitativa discreta)
 # VER PDF
+str(datos$Id)
+str(datos$MSSubClass)
+str(datos$MSZoning)
+str(datos$LotFrontage)
+str(datos$LotArea)
+str(datos$Street) 
+summary(datos$Street) 
+str(datos$Alley)
+summary(datos$Alley)
+str(datos$LotShape)
+summary(datos$LotShape)
+str(datos$LandContour)
+summary(datos$LandContour)
+str(datos$Utilities)
+summary(datos$Utilities)
+str(datos$LotConfig)
+summary(datos$LotConfig)
+str(datos$LandSlope)
+summary(datos$LandSlope)
+str(datos$Neighborhood)
+summary(datos$Neighborhood)
+str(datos$Condition1)
+summary(datos$Condition1)
+str(datos$Condition2)
+summary(datos$Condition2)
+str(datos$BldgType)
+summary(datos$BldgType)
+str(datos$HouseStyle)
+summary(datos$HouseStyle)
+str(datos$OverallQual)
+str(datos$OverallCond)
+str(datos$YearBuilt)
+str(datos$YearRemodAdd)
+str(datos$RoofStyle)
+summary(datos$RoofStyle)
+str(datos$RoofMatl)
+summary(datos$RoofMatl)
+str(datos$Exterior1st)
+summary(datos$Exterior1st)
+str(datos$Exterior2nd)
+summary(datos$Exterior2nd)
+str(datos$MasVnrType)
+summary(datos$MasVnrType)
+str(datos$MasVnrArea)
+str(datos$ExterQual)
+summary(datos$ExterQual)
+
+
 
 # 3. Aísle las variables numéricas de las categóricas, haga un análisis de correlación entre las mismas.
+
+nums<-dplyr::select_if(datos, is.numeric)   #NUMERICAS
+cats<-dplyr::select_if(datos,is.factor)     #CATEGORICAS
+
+View(datos$GarageYrBlt)
+
+cor(nums)
+
+
