178adffe51abdfbce6479ba737f3e8f943e2eee2
Sergio Marchena <mar16387@uvg.edu.gt>
2019-08-14 13:00:37 -0600

    first steps
    first steps

---
+++ lab1.R
@@ -0,0 +1,20 @@
+# LABORATORIO 1 DE DATA SCIENCE 
+# PABLO VIANA 16091
+# SERGIO MARCHENA 16387
+
+# Paquetes
+
+# Librerias
+
+# Datos
+datos <- read.csv("train.csv")
+
+# 1. Haga una exploración rápida de sus datos para eso haga un resumen de su dataset
+summary(datos)
+str(datos)
+head(datos)
+
+# 2. Diga el tipo de cada una de las variables del dataset (cualitativa o categórica, cuantitativa continua, cuantitativa discreta)
+# VER PDF
+
+# 3. Aísle las variables numéricas de las categóricas, haga un análisis de correlación entre las mismas.
